f8c72679381b5339d20a83fdbde17df30aa509cf
RavikantMorya <mauryaravikant555@gmail.com>
2025-04-12 22:44:51 +0530

    Aircraft Project

---
+++ src/test/java/com/example/MyAircraft/MyAircraftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MyAircraft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyAircraftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
