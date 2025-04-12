4949188d108fb3f5287456ecd40201eb37899731
RavikantMorya <mauryaravikant555@gmail.com>
2025-04-12 22:37:47 +0530

    Main Java File to Run Application

---
+++ src/main/java/com/example/MyAircraft/MyAircraftApplication.java
@@ -0,0 +1,13 @@
+package com.example.MyAircraft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyAircraftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyAircraftApplication.class, args);
+	}
+
+}
