6284cf6ecb12d3458d2238cb486984736ec75f6a
RavikantMorya <mauryaravikant555@gmail.com>
2025-04-13 01:07:35 +0530

    Modified The Controller to Take data from Consumer - PositionRetriever Class

--- src/main/java/com/example/MyAircraft/PositionController.java
+++ src/main/java/com/example/MyAircraft/PositionController.java
@@ -1,30 +1,49 @@
-package com.example.MyAircraft;
+package com.example.MyAircraft;//package com.example.MyAircraft;
 
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.reactive.function.client.WebClient;
 
+//
+//import lombok.NonNull;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.ui.Model;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.reactive.function.client.WebClient;
+//
+//@RequiredArgsConstructor
+//@Controller
+//public class PositionController {
+//    @NonNull
+//    private final AircraftRepository repository;
+//    private WebClient client =
+//            WebClient.create("http://localhost:7369/aircraft");
+//
+//    @GetMapping("/position")
+//    public String getCurrentAircraftPositions(Model model) {
+//        repository.deleteAll();
+//        client.get()
+//                .retrieve()
+//                .bodyToFlux(Aircraft.class)
+//                .filter(plane -> !plane.getReg().isEmpty())
+//                .toStream()
+//                .forEach(repository::save);
+//        model.addAttribute("aircrafts", repository.findAll());
+//        return "position";
+//    }
+//}
 @RequiredArgsConstructor
 @Controller
 public class PositionController {
     @NonNull
-    private final AircraftRepository repository;
-    private WebClient client =
-            WebClient.create("http://localhost:7369/aircraft");
-
+    private final AircraftRepository aircraftRepository;
     @GetMapping("/position")
     public String getCurrentAircraftPositions(Model model) {
-        repository.deleteAll();
-        client.get()
-                .retrieve()
-                .bodyToFlux(Aircraft.class)
-                .filter(plane -> !plane.getReg().isEmpty())
-                .toStream()
-                .forEach(repository::save);
-        model.addAttribute("aircrafts", repository.findAll());
+        model.addAttribute("aircrafts", aircraftRepository.findAll());
         return "position";
     }
+
 }
