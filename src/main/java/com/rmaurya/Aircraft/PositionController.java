1426746df7af7f85d087176cceb34fb30bf6a0a2
RavikantMorya <mauryaravikant555@gmail.com>
2025-04-26 22:49:53 +0530

    Converted From Controller to RestController to Test API via Junit and added /api/position

--- src/main/java/com/rmaurya/Aircraft/PositionController.java
+++ src/main/java/com/rmaurya/Aircraft/PositionController.java
@@ -5,18 +5,24 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @RequiredArgsConstructor
-@Controller
+//@Controller
+@RestController
 public class PositionController {
     @NonNull
     private final AircraftService aircraftService;
 
     @GetMapping("/position")
     public String getCurrentAircraftPositions(Model model) {
-        aircraftService.saveAircraftPosition();
         model.addAttribute("aircrafts",aircraftService.getAircraftPosition());
         return "position";
+    }
+
+    @GetMapping("/api/position")
+    public Iterable<Aircraft> getCurrentAircraftPositionsViaAPI() {
+    return  aircraftService.getAircraftPosition();
     }
 }
